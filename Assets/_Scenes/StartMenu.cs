fa91e39c1f9ae36142639b840d49646aeac8fe4e
Andrej <zarke998@gmail.com>
2020-08-21 12:09:42 +0200

    Add instructions panel in start menu

---
+++ Assets/_Scenes/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    void Awake(){
+        Initialize();
+    }  
+
+    void Initialize(){
+        PlayerStatistics.Instance.Load();
+    }
+
+    public void Instructions_Click(){
+        GameObject.Find("Start Menu").GetComponent<Canvas>().enabled = false;
+        GameObject.Find("InstructionsPanel").GetComponent<Canvas>().enabled = true;
+    }
+
+    public void InstructionsBack_Click(){
+        GameObject.Find("Start Menu").GetComponent<Canvas>().enabled = true;
+        GameObject.Find("InstructionsPanel").GetComponent<Canvas>().enabled = false;
+    }
+}
