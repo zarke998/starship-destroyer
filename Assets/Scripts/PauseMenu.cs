a8e10c85ede90c51210402c59e8f64897b9443c9
Andrej <zarke998@gmail.com>
2020-08-24 12:49:50 +0200

    Add pause menu in game scene

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool gamePaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(!gamePaused){
+                PauseGame();
+            }
+            else{
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0.0f;
+        
+        GetComponent<Canvas>().enabled = true;
+
+        gamePaused = true;
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1.0f;
+
+        GetComponent<Canvas>().enabled = false;
+
+        gamePaused = false;
+    }
+
+    public void MainMenuBtn_Click(){
+        ResumeGame();
+        GameObject.Find("LevelManager").GetComponent<LevelManager>().LoadLevel("Start Menu");
+    }
+}
